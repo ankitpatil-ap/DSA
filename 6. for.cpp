0e9b0878bf10bcc41cc7587b95011caccc6548f8
Ankit Patil <ankitpatil.v@gmail.com>
2023-06-17 21:19:36 +0530

    for first prog

--- 6. for.cpp
+++ 6. for.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Welcome to for loop";
+    cout<<"Welcome to for loop"<<endl;
+    for(int i=0; i<=10;i++){
+        cout<<i<<endl;
+    }
     return 0;
 }
