7bb2f028d608ce6ce2bafc435ad40c394f45bb3a
Ankit Patil <ankitpatil.v@gmail.com>
2023-06-18 21:17:10 +0530

    1000 Qs

--- 6.1 forexp1.cpp
+++ 6.1 forexp1.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    for (int i=0; i<=100; i++){
+    for (int i=0; i<=1000; i++){
         cout<<"Ankit"<<endl<<"Welcome to CP world."<<endl;
+        cout<<"You will solve 1000 All kinds of Programming Problems.";
     }
 }
